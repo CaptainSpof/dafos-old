b389df453a86961ffca7bc800683d457950b8382
CaptainSpof <captain.spof@gmail.com>
2021-12-26 18:14:14 +0100

    feat(daf): add dafpi ssh key

--- users/daf/default.nix
+++ users/daf/default.nix
@@ -15,6 +15,7 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOkMUUwRW95/DuanXq8qh3Jfjo5RIkKUvx3NPGc6P8A0 daf@dafbox"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP7YCmRYdXWhNTGWWklNYrQD5gUBTFhvzNiis5oD1YwV daf@daftop"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILGBJKhslXRQ4Bt8Nu3/YK799UsUpzpP6sDVkVw36nLR daf@dafpi"
     ];
     shell = "/etc/profiles/per-user/daf/bin/zsh";
     # passwordFile = "/run/secrets/daf";
