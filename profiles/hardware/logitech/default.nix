c1fa74beba05c47113efd3ee7e6f9f46370ece97
CaptainSpof <captain.spof@gmail.com>
2022-10-30 12:26:07 +0100

    feat(logitech): add option for gaming mice

--- profiles/hardware/logitech/default.nix
+++ profiles/hardware/logitech/default.nix
@@ -5,6 +5,7 @@ let cfg = config.profiles.hardware.logitech;
 in {
   options.profiles.hardware.logitech = {
     enable = mkOption { type = types.bool; default = false; };
+    gaming.enable = mkOption { type = types.bool; default = false; };
   };
 
   config = mkIf cfg.enable {
@@ -12,8 +13,11 @@ in {
     # Packages
     environment.systemPackages = with pkgs; [
       logiops
+    ] ++
+    (if cfg.gaming.enable then [
       piper
-    ];
+      libratbag
+    ] else []);
     services.udev.packages = [ pkgs.logitech-udev-rules ];
     services.udev.extraRules = ''
       KERNEL=="uinput", GROUP="input", MODE:="0660", OPTIONS+="static_node=uinput"
@@ -21,6 +25,7 @@ in {
     '';
 
     # Services
+    services.ratbagd.enable = cfg.gaming.enable;
     systemd.services.logid = {
       enable = true;
       description = "Logitech Configuration Daemon.";
