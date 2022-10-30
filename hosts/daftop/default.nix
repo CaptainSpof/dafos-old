46aa97ec3ee3e0fc78dc03d2797069630fe8ca76
CaptainSpof <captain.spof@gmail.com>
2022-10-30 12:26:07 +0100

    chore(i2c): cleanup i2c stuff
    originaly meant to fix my laptop speakers, but I couldn't make it work.
    I'll have to wait a potential fix in kernel 6.1

--- hosts/daftop/default.nix
+++ hosts/daftop/default.nix
@@ -10,40 +10,19 @@
       kernelModules = [ "amdgpu" ];
       luks.devices."crypted".device = "/dev/disk/by-uuid/2740b97b-a34c-43d5-9a5b-bb86521690ca";
     };
-    kernelModules = [ "tcp_bbr" "i2c-dev" "i2c-piix4" "kvm-amd" "uhid" ];
+    kernelModules = [ "tcp_bbr" "kvm-amd" "uhid" ];
     kernelPackages = pkgs.linuxPackages_latest;
     loader = {
       systemd-boot.enable = true;
       systemd-boot.editor = false;
       efi.canTouchEfiVariables = true;
     };
-      extraModprobeConfig = ''
-        options snd_hda_intel power_save=0 power_save_controller=N
-      '';
   };
-
-  # services.udev.extraRules = ''
-  #       KERNEL=="i2c-[0-9]*", GROUP="i2c", MODE="0660"
-  # '';
-
-  # systemd.user.services."yoga7-bass-speaker-on" = {
-  #   enable = true;
-  #   description = "yoga7 bass speaker on";
-  #   after = [ "pipewire.service" ];
-  #   wantedBy= ["pipewire.target"];
-  #   serviceConfig = {
-  #     ExecStart= "${pkgs.bash}/bin/bash -c '${pkgs.i2c-tools}/bin/i2cset -y 3 0x48 0x2 0 && ${pkgs.i2c-tools}/bin/i2cset -y 3 0x48 0x3 0'";
-  #   };
-  #   # script = ''
-  #   #   ${pkgs.i2c-tools}/bin/i2cset -y 3 0x48 0x2 0 && ${pkgs.i2c-tools}/bin/i2cset -y 3 0x48 0x3 0
-  #   # '';
-  # };
 
   # TODO: move out
   services.acpid.enable = true;
 
   environment.systemPackages = with pkgs; [
-    i2c-tools
     acpi
     lm_sensors
   ];
@@ -72,7 +51,6 @@
       enable = true;
       powerOnBoot = false;
     };
-    i2c.enable = true;
     cpu.amd.updateMicrocode = true;
     enableRedistributableFirmware = true;
     opengl.enable = true;
