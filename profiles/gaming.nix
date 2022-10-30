45cc5a89a91387361ee51ce6d60219bdb93f9047
CaptainSpof <captain.spof@gmail.com>
2022-10-30 21:25:50 +0100

    feat(gaming): add option to conditionally enable

--- profiles/gaming.nix
+++ profiles/gaming.nix
@@ -1,7 +1,14 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
-{
-  environment.defaultPackages = with pkgs; [ lutris];
-  programs.steam.enable = true;
-  hardware.steam-hardware.enable = true;
+with lib;
+let cfg = config.profiles.gaming;
+in {
+  options.profiles.gaming = {
+    enable = mkOption { type = types.bool; default = true; };
+  };
+  config = mkIf cfg.enable {
+    environment.defaultPackages = with pkgs; [ lutris];
+    programs.steam.enable = true;
+    hardware.steam-hardware.enable = true;
+  };
 }
