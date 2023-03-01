e49657f9e4090e94c71344946414ecfedf2cc81f
CaptainSpof <captain.spof@gmail.com>
2023-03-01 11:30:32 +0100

    feat(secrets): WIP add gh_key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,6 +3,9 @@ let
   daftop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP7YCmRYdXWhNTGWWklNYrQD5gUBTFhvzNiis5oD1YwV daf@daftop";
   dafbox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOkMUUwRW95/DuanXq8qh3Jfjo5RIkKUvx3NPGc6P8A0 daf@dafbox";
   dafpi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILGBJKhslXRQ4Bt8Nu3/YK799UsUpzpP6sDVkVw36nLR daf@dafpi";
+  all = [ daftop dafbox dafpi ];
 in
 {
+
+  "gh_key.age".publicKeys = all;
 }
