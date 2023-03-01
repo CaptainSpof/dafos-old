f1e6e88853d43f2c3dab44f80d203be73f9ba26f
CaptainSpof <captain.spof@gmail.com>
2023-03-01 11:15:38 +0100

    feat(ssh): update gh key

--- profiles/pc/ssh.nix
+++ profiles/pc/ssh.nix
@@ -1,15 +1,15 @@
 { config, ... }:
 
-let github_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHXgKC7XCa2PaLFBxPuN79jhreqTUZsSF3suI/c/EQ7M";
+let github_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINdlYbk5TRQj189jrlwwLqud4VuDPKxzuq/ns7rSuRbf captain.spof@gmail.com";
 in
 {
   home-manager.users."${config.vars.username}" = {
-    home.file.".ssh/gh-captainspof@daftop.pub".text = github_key;
+    home.file.".ssh/gh@captainspof.pub".text = github_key;
     programs.ssh = {
       enable = true;
       matchBlocks = {
         "github.com" = {
-          identityFile = "~/.ssh/gh-captainspof@daftop.pem";
+          identityFile = "~/.ssh/gh@captainspof.pem";
           identitiesOnly = true;
         };
         "gitlab.com" = {
